3a0059ddda71f35e0f6cfaecfc45b88c7348a5bd
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-04-30 11:37:38 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,13 @@ app.use(express.json());
 function CheckisOdd(req, res, next) {
 
     //Write Your Code here
+ var value = parseInt(req.query.num);
+    if( value%2 == 0 ){
+        req.query.isOdd = false;
+    }else{
+        req.query.isOdd = true;
+    }
+    next();
 
 }
 
